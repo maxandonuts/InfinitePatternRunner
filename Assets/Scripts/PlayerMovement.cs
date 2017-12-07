52c98d9fca0785b2b56317f94f536cb2f94e8cd3
Max R.R. Collada <mrdguezc@gmail.com>
2017-12-06 22:53:02 -0500

    Added basic movement and a test terrain

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _movementOffset = 1f;
+
+    private Vector3 movementVector = Vector3.one;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+}
